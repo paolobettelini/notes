43ffc3710f2efa73cf29fe8375d61821ce4d503a
paolobettelini <paolo.bettelini@samtrevano.ch>
2022-05-28 15:27:04 +0200

    western music notation

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "" ]; then
-  echo "Usage: $0 <title> [--bibtex]"
+  echo "Usage: $0 <title> [--bibtex] [--lilypond]"
   echo ""
   echo "--bibtex   Compile new bibtex references using biber"
   exit
@@ -12,11 +12,21 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 cd $SCRIPT_DIR/notes/$1
 
-lualatex *.tex
-
-if [ "$1" = "--bibtex" ]; then
+if [ "$2" = "--bibtex" ]; then
+    lualatex *.tex
     biber $1
     lualatex *.tex
+    lualatex *.tex
+elif [ "$2" = "--lilypond" ]; then
+    lilypond-book --pdf *.tex --out=target
+    lilypond-book --pdf *.tex --out=target
+    cd target
+    lualatex *.tex
+    mv *.pdf ../
+    cd ..
+    find . -type f -name "tmp*.pdf" -delete
+    find . -type f -name "tmp*.out" -delete
+else
     lualatex *.tex
 fi
 
