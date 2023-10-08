27c1c97956c4407143c45551f7de2a0ca4cbae38
paolobettelini <paolo.bettelini@samtrevano.ch>
2023-10-08 16:05:29 +0200

    Updated compile script

--- compile.sh
+++ compile.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "" ]; then
-  echo "Usage: $0 <title> [--bibtex] [--lilypond]"
-  echo ""
-  echo "--bibtex    Compile new bibtex references using biber"
-  echo "--lilypond  Compile using lilypond"
+  echo "Usage: $0 <title>"
   exit
 fi
 
@@ -13,23 +10,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 cd $SCRIPT_DIR/notes/$1
 
-if [ "$2" = "--bibtex" ]; then
-    lualatex *.tex
-    biber $1
-    lualatex *.tex
-    lualatex *.tex
-elif [ "$2" = "--lilypond" ]; then
-    lilypond-book --pdf *.tex --out=target
-    lilypond-book --pdf *.tex --out=target
-    cd target
-    lualatex *.tex
-    mv *.pdf ../
-    cd ..
-    find . -type f -name "tmp*.pdf" -delete
-    find . -type f -name "tmp*.out" -delete
-else
-    tectonic $1.tex -Z search-path=../../packages/
-fi
+tectonic $1.tex -Z search-path=../../packages/
 
 echo ""
 echo "Open your file at"
